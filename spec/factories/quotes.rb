09db05935705ea912887686079dce3c1ecdd31d9
dominicmacaulay <dominic.62502@gmail.com>
2024-07-01 18:49:45 -0400

    Complete chapter 2

--- spec/factories/quotes.rb
+++ spec/factories/quotes.rb
@@ -1,5 +1,14 @@
 FactoryBot.define do
   factory :quote do
-    name { 'Quote one!' }
+    name { 'Sample quote' }
+  end
+  factory :first_quote, parent: :quote do
+    name { 'First quote' }
+  end
+  factory :second_quote, parent: :quote do
+    name { 'Second quote' }
+  end
+  factory :third_quote, parent: :quote do
+    name { 'Third quote' }
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -42,7 +42,7 @@ RSpec.configure do |config|
   # compatibility in RSpec 3). It causes shared context metadata to be
   # inherited by the metadata hash of host groups and examples, rather than
   # triggering implicit auto-inclusion in groups with matching metadata.
-  config.shared_context_metadata_behavior = :apply_to_host_groups
+  # config.shared_context_metadata_behavior = :apply_to_host_groups
 
   # The settings below are suggested to provide a good initial experience
   # with RSpec, but feel free to customize to your heart's content.
@@ -57,6 +57,7 @@ RSpec.configure do |config|
   # Allows RSpec to persist some state between runs in order to support
   # the `--only-failures` and `--next-failure` CLI options. We recommend
   # you configure your source control system to ignore this file.
+  # config.example_status_persistence_file_path = 'spec/examples.txt'
 
   # Limits the available syntax to the non-monkey patched syntax that is
   # recommended. For more details, see:
