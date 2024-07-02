44a0b97e7c02dca088407c93e5841a5e5cffdb3b
dominicmacaulay <dominic.62502@gmail.com>
2024-07-02 11:13:19 -0400

    Complete chapter 4

--- spec/system/quotes_spec.rb
+++ spec/system/quotes_spec.rb
@@ -4,29 +4,21 @@ require 'rails_helper'
 
 RSpec.describe 'Quotes', type: :system do # rubocop:disable Metrics/BlockLength
   before do
-    driven_by(:rack_test)
+    # driven_by(:rack_test)
   end
 
   describe 'visits the quotes site' do # rubocop:disable Metrics/BlockLength
     let!(:quote) { create(:quote) }
-    # When we visit the Quotes#quotes page
-    # we expect to see a title with the text "Quotes"
+
     it 'creates a new quote' do
       visit quotes_path
       expect(page).to have_selector('h1', text: 'Quotes')
 
-      # When we click on the link with the text "New quote"
-      # we expect to land on a page with the title "New quote"
       click_on 'New quote'
-      expect(page).to have_selector('h1', text: 'New quote')
-
-      # When we fill in the name input with "Capybara quote"
-      # and we click on "Create Quote"
       fill_in 'Name', with: 'Capybara quote'
+      expect(page).to have_selector('h1', text: 'Quotes')
       click_on 'Create quote'
 
-      # We expect to be back on the page with the title "Quotes"
-      # and to see our "Capybara quote" added to the list
       expect(page).to have_selector('h1', text: 'Quotes')
       expect(page).to have_content('Capybara quote')
     end
@@ -35,20 +27,19 @@ RSpec.describe 'Quotes', type: :system do # rubocop:disable Metrics/BlockLength
       visit quotes_path
       click_on quote.name
 
-      expect(page).to have_css('h1', text: quote.name)
+      expect(page).to have_selector('h1', text: quote.name)
     end
 
     it 'Updating a quote' do
       visit quotes_path
-      expect(page).to have_css('h1', text: 'Quotes')
+      expect(page).to have_selector('h1', text: 'Quotes')
 
       click_on 'Edit', match: :first
-      expect(page).to have_css('h1', text: 'Edit quote')
-
       fill_in 'Name', with: 'Updated quote'
+      expect(page).to have_selector('h1', text: 'Quotes')
       click_on 'Update quote'
 
-      expect(page).to have_css('h1', text: 'Quotes')
+      expect(page).to have_selector('h1', text: 'Quotes')
       expect(page).to have_content('Updated quote')
     end
 
@@ -57,7 +48,7 @@ RSpec.describe 'Quotes', type: :system do # rubocop:disable Metrics/BlockLength
       expect(page).to have_content(quote.name)
 
       click_on 'Delete', match: :first
-      expect(page).not_to have_content quote.name
+      expect(page).not_to have_content(quote.name)
     end
   end
 end
